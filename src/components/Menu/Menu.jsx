49047ba8e9d88b896068388fbae4e09b83031e16
arpitavaddoriya <arpitavaddoriya@gmail.com>
2024-10-21 04:38:51 +0530

    images changes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -53,7 +53,7 @@ const Menu = () => {
         <div className="mad-col">
           <a href="#" className="mad-menu-item">
             <i className="mad-menu-icon">
-              <img className="svg" src="/src/assets/burger.svg" alt="Burgers" />
+              <img className="svg" src="/burger.svg" alt="Burgers" />
             </i>
             <h6 className="mad-menu-title">Burgers</h6>
           </a>

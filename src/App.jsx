a903ecc001dffb85aba9596c520b999472df9c9e
arpitavaddoriya <arpitavaddoriya@gmail.com>
2024-11-20 04:25:52 +0530

    change path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import PopupPage from "./components/PopupPage/PopupPage";
 import RevolutionSlider from "./components/RevolutionSlider/RevolutionSlider";
 import Section from "./components/section/Section";
 import WorkSection from "./components/WorkSection/WorkSection";
-import Footer from "./footer/footer";
+
 import Tab from "./Tab/Tab";
 import Testimonials from "./Testimonials/Testimonials";
 
